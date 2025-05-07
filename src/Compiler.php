2a5d72dca038b6b3c8bb401d363b183112ed4575
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-05-07 11:41:15 +0200

    initial

---
+++ src/Compiler.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tualo\Office\PostmanAPI;
+
+use Tualo\Office\Basic\TualoApplication;
+use Tualo\Office\ExtJSCompiler\ICompiler;
+use Tualo\Office\ExtJSCompiler\CompilerHelper;
+
+class Compiler implements ICompiler
+{
+    public static function getFiles()
+    {
+        return CompilerHelper::getFiles(__DIR__, 'postman-api', 10003);
+    }
+}
---
+++ src/functions.php
@@ -0,0 +1,7 @@
+<?php
+# require_once "Compiler.php";
+# require_once "Checks/Tables.php";
+# require_once "Checks/StoredProcedures.php";
+# require_once "Commandline/Install.php";
+
+require_once __DIR__ . '/Routes/Test.php';
