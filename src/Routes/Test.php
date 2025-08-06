6703c15e84752e230b4c9ac5fd710e88b8f8cb74
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-08-06 14:32:44 +0200

    fix ds store

---
+++ src/Routes/Test.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tualo\Office\PostmanAPI\Routes;
+
+use Tualo\Office\Basic\TualoApplication as App;
+use Tualo\Office\Basic\Route;
+use Tualo\Office\Basic\IRoute;
+use WebScientist\Postman\Services\PostmanService as Postman;
+
+class Test implements IRoute
+{
+
+    public static function register()
+    {
+
+        Route::add('/postman-api/test', function () {
+            $db = App::get('session')->getDB();
+            App::contenttype('application/json');
+            try {
+
+                $postman = new Postman();
+                $collection = $postman->collection('postmanapi');
+                $req = $collection->request('postmanapi test', 'GET');
+                $req->url('postman-api/test');
+
+                App::result('collection', $collection->jsonSerialize());
+            } catch (\Exception $e) {
+                App::result('msg', $e->getMessage());
+            }
+        }, array('get'), true);
+    }
+}
